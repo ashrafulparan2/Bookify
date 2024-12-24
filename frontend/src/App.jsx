40c775ca73ef73cdfcec659c5f765354bf345e96
Symom Hossain Shohan <shshohan0000@gmail.com>
2024-12-24 15:14:47 +0600

    add Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,16 @@
-
-import { Outlet } from "react-router-dom"
-
+import { Outlet } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
 function App() {
-
-
   return (
     <>
-    <nav>Navbar</nav>
-    <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6">
-      <Outlet />
-    </main>
-    <footer>Footer</footer>
-
+      <Navbar />
+      <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6">
+        <Outlet />
+      </main>
+      <footer>Footer</footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
