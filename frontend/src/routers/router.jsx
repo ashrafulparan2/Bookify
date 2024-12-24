52e9ed86bf819ae2476f9003355200dcdef04705
Sajid064 <sajid.chowdhury009@gmail.com>
2024-12-24 16:04:28 +0600

    Login Register Navbar

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,5 +1,7 @@
 import {createBrowserRouter,} from "react-router-dom";
 import App from "../App";
+import Login from "../components/Login";
+import Register from "../components/Register";
 
 const router = createBrowserRouter([
     {
@@ -20,11 +22,11 @@ const router = createBrowserRouter([
         },
         {
             path: "/login",
-            element: <div>Login</div>
+            element: <Login/>
         },
         {
           path: "/register",
-          element: <div>Register</div>
+          element: <Register/>
         }
       ]
     },
