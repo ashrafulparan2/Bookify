8264155d6196212392028f1d40766de1593f2ab6
5hawon <sshawon993@gmail.com>
2024-12-27 19:12:53 +0600

    Update home page banner content

--- frontend/src/pages/home/Banner.jsx
+++ frontend/src/pages/home/Banner.jsx
@@ -10,8 +10,13 @@ const Banner = () => {
         </div>
         
         <div className='md:w-1/2 w-full'>
-            <h1 className='md:text-5xl text-2xl font-medium mb-7'>New Releases This Week</h1>
-            <p className='mb-10'>It's time to update your reading list with some of the latest and greatest releases in the literary world. From heart-pumping thrillers to captivating memoirs, this week's new releases offer something for everyone</p>
+        <h2 className="md:text-3xl text-2xl font-medium mb-7 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
+    পাঠে নতুন দিগন্ত, বইয়ের সাথে বন্ধুত্ব
+</h2>
+<p className="mb-10">
+    আপনার প্রিয় বই এখন এক ক্লিকে – সহজে বই কিনুন, নতুন বইয়ের কালেকশন আবিষ্কার করুন এবং সেরা অফারে আপনার বুকশেলফ সাজান আমাদের বুকস্টোর থেকে!
+</p>
+
 
             <button 
   style={{
