755c35ee98be7b98d26d5b5f1950cdd6444f089a
Symom Hossain Shohan <shshohan0000@gmail.com>
2024-12-31 14:56:44 +0600

    Update TopSellers component to include new Bengali categories and improve formatting

--- frontend/src/Pages/home/TopSellers.jsx
+++ frontend/src/Pages/home/TopSellers.jsx
@@ -1,38 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import BookCard from '../books/BookCard';
+import React, { useEffect, useState } from "react";
+import BookCard from "../books/BookCard";
 
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // import required modules
-import { Pagination, Navigation } from 'swiper/modules';
+import { Pagination, Navigation } from "swiper/modules";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { useFetchAllBooksQuery } from '../../redux/features/books/booksApi';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { useFetchAllBooksQuery } from "../../redux/features/books/booksApi";
 
-const categories = ["Choose a genre", "Business", "Fiction", "Horror", "Adventure"]
+const categories = [
+  "Choose a genre",
+  "ইতিহাস ও ঐতিহ্য",
+  "উপন্যাস",
+  "গণিত, বিজ্ঞান ও প্রযুক্তি",
+  "ছড়া, কবিতা ও আবৃত্তি",
+  "থ্রিলার",
+  "ধর্মীয়",
+  "প্রবন্ধ",
+];
 
 const TopSellers = () => {
-    
-    const [selectedCategory, setSelectedCategory] = useState("Choose a genre");
+  const [selectedCategory, setSelectedCategory] = useState("Choose a genre");
 
-   const {data: books = []} = useFetchAllBooksQuery();
-  
-    const filteredBooks = selectedCategory === "Choose a genre" ? books : books.filter(book => book.category === selectedCategory.toLowerCase())
+  const { data: books = [] } = useFetchAllBooksQuery();
 
-    return (
-        <div className='py-10'>
-            <h2 className='text-3xl font-semibold mb-6'>Top Sellers</h2>
-            {/* category filtering */}
-            <div className='mb-8 flex items-center'>
-                <select
-                    onChange={(e) => setSelectedCategory(e.target.value)}
-                    name="category"
-                    id="category"
-                    className='
+  const filteredBooks =
+    selectedCategory === "Choose a genre"
+      ? books
+      : books.filter(
+          (book) => book.category === selectedCategory.toLowerCase()
+        );
+
+  return (
+    <div className="py-10">
+      <h2 className="text-3xl font-semibold mb-6">Top Sellers</h2>
+      {/* category filtering */}
+      <div className="mb-8 flex items-center">
+        <select
+          onChange={(e) => setSelectedCategory(e.target.value)}
+          name="category"
+          id="category"
+          className="
                         border 
                         border-gray-300 
                         rounded-full 
@@ -54,59 +67,50 @@ const TopSellers = () => {
                         focus:bg-gradient-to-r 
                         focus:from-[#1d3557] 
                         focus:to-[#457b9d] 
-                        focus:text-white'
-                >
-                    {
-                        categories.map((category, index) => (
-                            <option key={index} value={category} className="text-gray-600">
-                                {category}
-                            </option>
-                        ))
-                    }
-                </select>
-            </div>
+                        focus:text-white"
+        >
+          {categories.map((category, index) => (
+            <option key={index} value={category} className="text-gray-600">
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
 
-            <Swiper
-                slidesPerView={1}
-                spaceBetween={30}
-                navigation={true}
-                breakpoints={{
-                    640: {
-                        slidesPerView: 1,
-                        spaceBetween: 20,
-                    },
-                    768: {
-                        slidesPerView: 2,
-                        spaceBetween: 40,
-                    },
-                    1024: {
-                        slidesPerView: 2,
-                        spaceBetween: 50,
-                    },
-                    1180: {
-                        slidesPerView: 3,
-                        spaceBetween: 50,
-                    }
-                }}
-                modules={[Pagination, Navigation]}
-                className="mySwiper"
-            >
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        navigation={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1180: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {filteredBooks.length > 0 &&
+          filteredBooks.map((book, index) => (
+            <SwiperSlide key={index}>
+              <BookCard book={book} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </div>
+  );
+};
 
-                {
-                   filteredBooks.length > 0 && filteredBooks.map((book, index) => (
-                        <SwiperSlide key={index}>
-                            <BookCard  book={book} />
-                        </SwiperSlide>
-                    ))
-                }
-
-
-
-            </Swiper>
-
-
-        </div>
-    )
-}
-
-export default TopSellers
+export default TopSellers;
