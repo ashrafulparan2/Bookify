2f6e1214fc8bc17a807ded921effbd13ad634e4b
anik5099 <anikshanto5099@gmail.com>
2025-01-01 11:54:28 +0600

    user dashboard

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,25 +1,26 @@
-import { createBrowserRouter, } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-
-import Home from "../pages/home/Home";
-
 import AdminLogin from "../components/AdminLogin";
 import Login from "../components/Login";
 import Register from "../components/Register";
-import CartPage from "../Pages/books/CartPage";
-import CheckoutPage from "../Pages/books/CheckoutPage";
+import CartPage from "../pages/books/CartPage";
+import CheckoutPage from "../pages/books/CheckoutPage";
 import OrderPage from "../pages/books/OrderPage";
-import SingleBook from "../Pages/books/SingleBook";
-import AddBook from "../Pages/dashboard/addBook/AddBook";
-import UpdateBook from "../Pages/dashboard/EditBook/UpdateBook";
+import SingleBook from "../pages/books/SingleBook";
+import AddBook from "../pages/dashboard/addBook/AddBook";
+import Dashboard from "../pages/dashboard/Dashboard";
+import DashboardLayout from "../pages/dashboard/DashboardLayout";
+import UpdateBook from "../pages/dashboard/EditBook/UpdateBook";
+import ManageBooks from "../pages/dashboard/manageBooks/ManageBooks";
+import UserDashboard from "../pages/dashboard/users/UserDashboard";
+import Home from "../pages/home/Home";
 import AdminRoute from "./AdminRoute";
 import PrivateRoute from "./PrivateRoute";
-
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <App />,
+      element: <App/>,
       children: [
         {
             path: "/",
@@ -27,64 +28,73 @@ const router = createBrowserRouter([
         },
         {
             path: "/orders",
-            element: <PrivateRoute><OrderPage/></PrivateRoute>,
+            element: <PrivateRoute><OrderPage/></PrivateRoute>
         },
         {
             path: "/about",
-            element: <div>About</div>,
+            element: <div>About</div>
         },
         {
-            path: "/login",
-            element: <Login/>
+          path: "/login",
+          element: <Login/>
         },
         {
           path: "/register",
           element: <Register/>
         },
         {
-          path: "/checkout",
-          element: <PrivateRoute> <CheckoutPage/> </PrivateRoute> 
-        },
-        {
           path: "/cart",
           element: <CartPage/>
         },
         {
+          path: "/checkout",
+          element: <PrivateRoute><CheckoutPage/></PrivateRoute>
+        },
+        {
           path: "/books/:id",
           element: <SingleBook/>
+        },
+        {
+          path: "/user-dashboard",
+          element: <PrivateRoute><UserDashboard/></PrivateRoute>
         }
+        
       ]
     },
     {
       path: "/admin",
-      element: <AdminLogin/>,
+      element: <AdminLogin/>
     },
     {
       path: "/dashboard",
-      element: <AdminRoute> <div>Admin Dashboard </div> </AdminRoute> ,
-      children: [
+      element: <AdminRoute>
+        <DashboardLayout/>
+      </AdminRoute>,
+      children:[
         {
           path: "",
-          element: <AdminRoute> <div>Dashboard Home</div> </AdminRoute>,
-
+          element: <AdminRoute><Dashboard/></AdminRoute>
         },
         {
           path: "add-new-book",
-          element: <AdminRoute> <AddBook/> </AdminRoute>,
-
+          element: <AdminRoute>
+            <AddBook/>
+          </AdminRoute>
         },
         {
           path: "edit-book/:id",
-          element: <AdminRoute> <UpdateBook/> </AdminRoute>,
-
+          element: <AdminRoute>
+            <UpdateBook/>
+          </AdminRoute>
         },
         {
-          path: "manage-book",
-          element: <AdminRoute> <div>Manage Book </div> </AdminRoute>,
-
+          path: "manage-books",
+          element: <AdminRoute>
+            <ManageBooks/>
+          </AdminRoute>
         }
-
       ]
     }
   ]);
+
   export default router;
