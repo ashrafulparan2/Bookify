b023f9cc5037e806a7c4741c6146481dd024c78e
ashrafulparan2 <ashrafulparan@gmail.com>
2025-01-02 12:27:40 +0600

    Add Vercel configuration and update CORS settings for deployment

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://bookify-kpyge4twy-ashraful-islam-parans-projects.vercel.app/'],
     credentials: true
 }))
 
